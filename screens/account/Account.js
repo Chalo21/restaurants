9db98433f75482fb2835ec047305be3f3752f7ce
Chalo21 <chaloarroyave21@gmail.com>
2021-03-01 22:23:28 -0500

    Navigate to login page

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet } from 'react-native'
-import { getCurrentUser } from '../../utils/actions'
+import { isUserLogged } from '../../utils/actions'
 
 import Loading from '../../components/Loading'
 import UserGuest from './UserGuest'
@@ -10,8 +10,7 @@ export default function Account() {
     const [login, setLogin] = useState(null)
     
     useEffect(() => {
-        const user = getCurrentUser()
-        user ? setLogin(true) : setLogin(false)
+        setLogin(isUserLogged)
     }, [])
 
     if(login == null) {
--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
 import { Button } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 export default function UserGuest() {
+    const navigation = useNavigation()
+
     return (
         <ScrollView
             centerContent
@@ -21,7 +24,7 @@ export default function UserGuest() {
             <Button 
                 buttonStyle={styles.button}
                 title="Ver tu perfil"
-                onPress={() => console.log("Click!!!")}
+                onPress={() => navigation.navigate("login")}
             />
         </ScrollView>
     )
@@ -35,8 +38,7 @@ const styles = StyleSheet.create({
     image : {
         height: 300,
         width: "100%",
-        marginBottom: 10,
-        textAlign: "center"
+        marginBottom: 10
     },
 
     title : {

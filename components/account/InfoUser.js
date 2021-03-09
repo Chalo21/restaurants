586ebe26f0815b7a27822795d56f61e7eec664d0
Chalo21 <chaloarroyave21@gmail.com>
2021-03-09 15:56:21 -0500

    continuing user screen

--- components/account/InfoUser.js
+++ components/account/InfoUser.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
 
 export default function InfoUser({ user }) {
+    console.log(user)
     return (
-        <View>
-            <Text>InfoUser...</Text>
+        <View style={styles.continer}>
+            <Avatar 
+                rounded
+                size="large"
+                source={
+                    user.photoURL 
+                        ? { url:photoURL }
+                        : require("../../assets/avatar-default.jpg")
+                }
+            />
+            <View style={styles.infoUser}>
+                <Text style={styles.displayName}>
+                    {
+                        user.displayName ? user.displayName : "Anónimo" 
+                    }
+                </Text>
+                <Text>{user.email}</Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    continer : {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "#f9f9f9",
+        paddingVertical: 30
+    },
+
+    infoUser : {
+        marginLeft: 20
+    },
+
+    displayName : {
+        fontWeight: "bold",
+        paddingBottom: 5
+    }
+})

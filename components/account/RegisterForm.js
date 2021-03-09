84e47312c378584f115e50ff6b669f14c203c69c
Chalo21 <chaloarroyave21@gmail.com>
2021-03-09 14:18:27 -0500

    Login UI

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,6 +1,6 @@
 import { size } from 'lodash'
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Button, Icon, Input } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 

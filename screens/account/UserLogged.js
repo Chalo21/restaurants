164d9413f9039d27cc6fc13094c91ac35cffc1f3
Chalo21 <chaloarroyave21@gmail.com>
2021-04-03 19:38:32 -0500

    user options

--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -7,6 +7,7 @@ import Toast from 'react-native-easy-toast'
 import { closeSession, getCurrentUser } from '../../utils/actions'
 import Loading from '../../components/Loading'
 import InfoUser from '../../components/account/InfoUser'
+import AccountOptions from '../../components/account/AccountOptions'
 
 export default function UserLogged() {
     const toastRef = useRef()
@@ -30,10 +31,13 @@ export default function UserLogged() {
                             setLoading={setLoading} 
                             setLoadingText={setLoadingText}
                         />
+                        <AccountOptions
+                            user={user}
+                            toastRef={toastRef}
+                        />
                     </View>
                 )
             }
-            <Text>Account Options</Text>
             <Button
                 title="Cerrar Sesión"
                 buttonStyle={styles.btnCloseSesion}

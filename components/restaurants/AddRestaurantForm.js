2e64368b5b4596b4e90c06e67c0d4e8b01178093
Chalo21 <chaloarroyave21@gmail.com>
2021-04-06 16:19:30 -0500

    Upload Images to firebase

--- components/restaurants/AddRestaurantForm.js
+++ components/restaurants/AddRestaurantForm.js
@@ -4,9 +4,11 @@ import { Avatar, Button, Icon, Input, Image } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
 import { map, size, filter, isEmpty } from 'lodash'
 import MapView from 'react-native-maps'
+import uuid from 'random-uuid-v4'
 
 import { getCurrentLocation, loadImageFromGallery, validateEmail } from '../../utils/helpers'
 import Modal from '../../components/Modal'
+import { uploadImage } from '../../utils/actions'
 
 const widthScreen = Dimensions.get("window").width
 
@@ -21,11 +23,30 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation}) {
     const [isVisibleMap, setIsVisibleMap] = useState(false)
     const [locationRestaurant, setLocationRestaurant] = useState(null)
 
-    const AddRestaurant = () => {
+    const AddRestaurant = async() => {
         if(!validForm()){
             return
         }
+
+        setLoading(true)
+        const response = await uploadImages()
+        console.log(response)
+        setLoading(false)
+
         console.log("Meloooooooooo")
+    }
+
+    const uploadImages = async () => {
+        const imagesUrl = []
+        await Promise.all(
+            map(imagesSelected, async(image) => {
+                const response = await uploadImage(image, "restaurants", uuid())
+                if(response.statusResponse){
+                    imagesUrl.push(response.url)
+                }
+            })
+        )
+        return imagesUrl
     }
 
     const validForm = () => {

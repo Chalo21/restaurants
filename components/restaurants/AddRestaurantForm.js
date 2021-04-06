61fed6ff67ab2147686c6319ea261ebb95b4c827
Chalo21 <chaloarroyave21@gmail.com>
2021-04-05 22:37:12 -0500

    Establishing the main image of the restaurant

--- components/restaurants/AddRestaurantForm.js
+++ components/restaurants/AddRestaurantForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import { StyleSheet, ScrollView, View, Alert } from 'react-native'
-import { Avatar, Button, Icon, Input } from 'react-native-elements'
+import { StyleSheet, ScrollView, View, Alert, Dimensions } from 'react-native'
+import { Avatar, Button, Icon, Input, Image } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
 import { map, size, filter } from 'lodash'
 
 import { loadImageFromGallery } from '../../utils/helpers'
+
+const widthScreen = Dimensions.get("window").width
 
 export default function AddRestaurantForm({ toastRef, setLoading, navigation}) {
     const [formData, setFormData] = useState(defaultFormValues())
@@ -20,7 +22,10 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation}) {
         console.log("Meloooooooooo")
     }
     return (
-        <View style={styles.viewContainer}>
+        <ScrollView style={styles.viewContainer}>
+            <ImageRestaurant
+                imageRestaurant={imagesSelected[0]}
+            />
             <FormAdd
                 formData={formData}
                 setFormData={setFormData}
@@ -40,6 +45,22 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation}) {
                 onPress={AddRestaurant}
                 buttonStyle={styles.btnAddRestaurant}
             />
+        </ScrollView>
+    )
+}
+
+function ImageRestaurant({ imageRestaurant }) {
+    return(
+        <View style={styles.viewPhoto}>
+            <Image
+                style={{ width: widthScreen, height: 200}}
+                source={
+                    imageRestaurant
+                        ? { uri: imageRestaurant}
+                        : require("../../assets/no-image.png")
+                }
+            />
+
         </View>
     )
 }
@@ -221,5 +242,10 @@ const styles = StyleSheet.create({
         width: 70,
         height: 70,
         marginRight: 10
+    },
+    viewPhoto : {
+        alignItems: "center",
+        height: 200,
+        marginBottom: 20
     }
 })

942392ac3deba7b2231ea82e83f3c174c102907d
Chalo21 <chaloarroyave21@gmail.com>
2021-04-05 20:46:01 -0500

    AddRestaurant Form

--- components/restaurants/AddRestaurantForm.js
+++ components/restaurants/AddRestaurantForm.js
@@ -1,12 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, Input } from 'react-native-elements'
+import CountryPicker from 'react-native-country-picker-modal'
 
-export default function AddRestaurantForm({ toastRef, setLoading}) {
+export default function AddRestaurantForm({ toastRef, setLoading, navigation}) {
+    const AddRestaurant = () => {
+        console.log("Meloooooooooo")
+    }
     return (
-        <View>
-            <Text>AddRestaurantForm...</Text>
+        <View style={styles.viewContainer}>
+            <FormAdd/>
+            <Button
+                title="Crear Restaurante"
+                onPress={AddRestaurant}
+                buttonStyle={styles.btnAddRestaurant}
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+function FormAdd() {
+    const [country, setCountry] = useState("CO")
+    const [callingCode, setCallingCode] = useState("57")
+    const [phone, setPhone] = useState("")
+
+    return (
+        <View style={styles.viewForm}>
+            <Input
+                placeholder="Nombre del restaurante..."
+            />
+            <Input
+                placeholder="Dirección del restaurante..."
+            />
+            <Input
+                placeholder="Email del restaurante..."
+                keyboardType="email-address"
+            />
+            <View style={styles.phoneView}>
+                <CountryPicker
+                    withFlag
+                    withCallingCode
+                    withFilter
+                    withCallingCodeButton
+                    containerStyle={styles.countryPicker}
+                    countryCode={country}
+                    onSelect={(country) => {
+                        setCountry(country.cca2)
+                        setCallingCode(country.callingCode[0])
+                    }}
+                />
+                <Input
+                    placeholder="WhatsApp del restaurante..."
+                    keyboardType="phone-pad"
+                    containerStyle={styles.inputPhone}
+                />
+            </View>
+                <Input
+                    placeholder="Descripción del restaurante..."
+                    multiline
+                    containerStyle={styles.textArea}
+                />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    viewContainer : {
+        height:"100%"
+    },
+    viewForm : {
+        marginHorizontal: 10,
+    },
+    textArea : {
+        height: 100,
+        width: "100%"
+    },
+    phoneView : {
+        width: "80%",
+        flexDirection: "row"
+    },
+    inputPhone : {
+        width: "80%"
+    },
+    btnAddRestaurant :{
+        margin: 20,
+        backgroundColor:"#6d1a1a"
+    }
+})
--- screens/restaurants/AddRestaurant.js
+++ screens/restaurants/AddRestaurant.js
@@ -15,6 +15,7 @@ export default function AddRestaurant({ navigation }) {
             <AddRestaurantForm
                 toastRef={toastRef}
                 setLoading={setLoading}
+                navigation={navigation}
             />
             <Loading
                 isVisible={loading}

42e83c9689e9a42fc1c9d322b7219e9cbe3895f6
Chalo21 <chaloarroyave21@gmail.com>
2021-07-16 23:16:31 -0500

    Login with Google

--- components/account/LoginForm.js
+++ components/account/LoginForm.js
@@ -1,8 +1,11 @@
+import React, { useState } from 'react'
+import { StyleSheet, View, Platform, Alert } from 'react-native'
+import { Button, Icon, Input } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 import { isEmpty } from 'lodash'
-import React, { useState } from 'react'
-import { StyleSheet, View } from 'react-native'
-import { Button, Icon, Input } from 'react-native-elements'
+
+import * as GoogleSingIn from 'expo-google-sign-in'
+import * as firebase from 'firebase'
 
 import { loginWithEmailAndPassword } from '../../utils/actions'
 import { validateEmail } from '../../utils/helpers'
@@ -21,6 +24,69 @@ export default function LoginForm() {
 
     const onChange = (e, type) => {
         setFormData({...formData, [type]: e.nativeEvent.text})
+    }
+
+    async function googleSignInAsync() {
+        try {
+            await GoogleSignIn.initAsync()
+            if (Platform.OS === "android") {
+                await GoogleSignIn.askForPlayServicesAsync()
+            }
+            const { type, user } = await GoogleSignIn.signInAsync()
+            if (type === "success") {
+                onSignIn(user)
+                setLoading(false)
+                return true
+            } else {
+                setLoading(false)
+                Alert.alert(JSON.stringify(result))
+                return { cancelled: true }
+            }
+        } catch (error) {
+            setLoading(false)
+            Alert.alert(error.message)
+            return { error: true }
+        }
+    }
+      
+    function onSignIn(googleUser) {
+        const unsubscribe = firebase
+            .auth()
+            .onAuthStateChanged(function (firebaseUser) {
+                unsubscribe()
+                if (!isUserEqual(googleUser, firebaseUser)) {
+                    const credential = firebase.auth.GoogleAuthProvider.credential(
+                        googleUser.auth.idToken,
+                        googleUser.auth.accessToken
+                    )
+                    setLoading(true);
+                    firebase
+                        .auth()
+                        .signInWithCredential(credential)
+                        .then(() => {
+                            setLoading(false)
+                        })
+                        .catch(function (error) {
+                            setLoading(false)
+                            Alert.alert(error.message)
+                        })
+                } else {
+                    Alert.alert("Usuario ya está logueado")
+                }
+            });
+    }
+      
+    function isUserEqual(googleUser, firebaseUser) {
+        if (firebaseUser) {
+            let providerData = firebaseUser.providerData
+            for (let i = 0; i < providerData.length; i++) {
+                if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     const doLogin = async() => {
@@ -92,6 +158,21 @@ export default function LoginForm() {
                 buttonStyle={styles.btn}
                 onPress={() => doLogin()}
             />
+            <Button
+                title="Iniciar Sesión con Google"
+                containerStyle={styles.btnContainer}
+                buttonStyle={styles.btnGoogle}
+                onPress={googleSignInAsync}
+                icon={
+                    <Icon
+                        name="google"
+                        type="material-community"
+                        marginRight= {10}
+                        size={20}
+                        color= "#FFFFFF"
+                    />
+                }
+            />
             <Loading isVisible={loading} text="Iniciando Sesión..."/> 
         </View>
     )
@@ -125,5 +206,9 @@ const styles = StyleSheet.create({
 
     icon : {
         color: "#84bcbc"
+    },
+
+    btnGoogle : {
+        backgroundColor: "#ea4335"
     }
 })
